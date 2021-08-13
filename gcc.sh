8cd29520474de71d3235ee3ada3823b424c3bb1d
WhysDev <78435081+WhysDev@users.noreply.github.com>
2021-08-14 01:14:03 +0700

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,69 +1,166 @@
-#!/bin/bash
+#! /bin/bash
 
-export KERNELNAME="SaranRomKak"
+ # Script For Building Android arm64 Kernel
+ #
+ # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
 
-export LOCALVERSION=SRK-Kernel-V21
+#Kernel building script
 
-export KBUILD_BUILD_USER=WhysDev
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
 
-export KBUILD_BUILD_HOST=WhysCI
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+    exit 1
+}
 
-export TOOLCHAIN=gcc
+##------------------------------------------------------##
+##----------Basic Informations, COMPULSORY--------------##
 
-export DEVICES=whyred,tulip,lavender,a26x
+# The defult directory where the kernel should be placed
+KERNEL_DIR=$PWD
 
-export CI_ID=-1001287929514
+# The name of the Kernel, to name the ZIP
+ZIPNAME="SaranRomKak-Kernel-V21"
 
-export GROUP_ID=-1001415355277
+# The codename of the device
+DEVICE="whyred"
 
-source helper
+# The defconfig which should be used. Get it from config.gz from
+# your device or check source
+DEFCONFIG=whyred_defconfig
+DEFCONFIG=whyred-newcam_defconfig
 
-gen_toolchain
+##------------------------------------------------------##
+##---------Do Not Touch Anything Beyond This------------##
 
-send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+## Set defaults first
+CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+token=$TELEGRAM_TOKEN
+export KBUILD_BUILD_HOST CI_BRANCH
 
-send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+## Export CI Env
+export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
+export CI_BRANCH=$DRONE_BRANCH
+export CHATID="-1001287929514"
 
-START=$(date +"%s")
+#Check Kernel Version
+KERVER=$(make kernelversion)
 
-for i in ${DEVICES//,/ }
 
-do
+# Set a commit head
+COMMIT_HEAD=$(git log --oneline -1)
 
-	build ${i} -oldcam
+#Now Its time for other stuffs like cloning, exporting, etc
 
-	build ${i} -newcam
+ clone() {
+	echo " "
+		msg "|| Cloning GCC ||"
+		git clone --depth=1 https://github.com/pjorektneira/arm64-gcc -b arm64 gcc64
+		git clone --depth=1 https://github.com/pjorektneira/arm32-gcc -b arm gcc32
+		GCC64_DIR=$KERNEL_DIR/gcc64
+		GCC32_DIR=$KERNEL_DIR/gcc32
 
-done
+	msg "|| Cloning Anykernel ||"
+	git clone --depth 1 https://github.com/WhysDev/AnyKernel3-2
+}
 
-send_msg "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+##------------------------------------------------------##
 
-send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+exports() {
+	export KBUILD_BUILD_USER="SRK"
+	export KBUILD_BUILD_HOST="WhysCI"
+	export ARCH=arm64
+	export SUBARCH=arm64
 
-git apply oc.patch
+	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+	PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
 
-git apply em.patch
+	export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-none-eabi-
+	export CROSS_COMPILE=$GCC64_DIR/bin/aarch64-none-elf-
+	export PATH KBUILD_COMPILER_STRING
+	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+	export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
+	PROCS=$(nproc --all)
+	export PROCS
+}
 
-for i in ${DEVICES//,/ }
+##---------------------------------------------------------##
 
-do
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="-1001403511595" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
 
-	if [ $i == "whyred" ] || [ $i == "tulip" ]
+}
 
-	then
+##----------------------------------------------------------------##
 
-		build ${i} -oldcam -overclock
+tg_post_build() {
+	#Show the Checksum alongwith caption
+	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+	-F chat_id="$2"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3 | <code>Build Number : </code><b>$DRONE_BUILD_NUMBER</b>"
+}
 
-		build ${i} -newcam -overclock
+##----------------------------------------------------------##
 
-	fi
+build_kernel() {
 
-done
+	tg_post_msg "<b>🔨 $KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>" "$CHATID"
 
-END=$(date +"%s")
+	msg "|| Started Compilation ||"
+	BUILD_START=$(date +"%s")
+	make O=out $DEFCONFIG
+	make -j"$PROCS" O=out LD=ld.lld
 
-DIFF=$(( END - START ))
+		BUILD_END=$(date +"%s")
+		DIFF=$((BUILD_END - BUILD_START))
 
-send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+		if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
+	    then
+	    	msg "|| Kernel successfully compiled ||"
+				gen_zip
+		else
+		tg_post_msg "<b>❌ Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>" "$CHATID"
+		fi
 
-send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+}
+
+##--------------------------------------------------------------##
+
+gen_zip() {
+	msg "|| Zipping into a flashable zip ||"
+	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
+	cd AnyKernel3 || exit
+	zip -r9 $ZIPNAME-$DEVICE-"$DRONE_BUILD_NUMBER" ./* -x .git README.md
+
+	## Prepare a final zip variable
+	ZIP_FINAL="$ZIPNAME-$DEVICE-$DRONE_BUILD_NUMBER.zip"
+	tg_post_build "$ZIP_FINAL" "$CHATID" "✅ Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+	cd ..
+}
+
+clone
+exports
+build_kernel
+
+##----------------*****-----------------------------##
